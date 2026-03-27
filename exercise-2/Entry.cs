267447eaac37779cb664a01ddba836b99262241a
caffeine-abuser <s28177@pjwstk.edu.pl>
2026-03-27 21:08:19 +0100

    gigasquash: general housekeeping
    ...and moving things around

---
+++ exercise-2/Entry.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+// deserialize data from DB
+var database = new DataSource("Data Source=hardware-rental.sqlite");
+var users = DbInterop.loadUsers();
+var devices = DbInterop.loadDevices();
+var rentals = DbInterop.loadRentals();
+
+// check internal consistency; drop invalid elements
+
+
+// actually accept arguments from user
+// todo: add new user/device, list everything
